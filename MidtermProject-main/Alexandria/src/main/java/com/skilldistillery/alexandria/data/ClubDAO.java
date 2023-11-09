82d2c844074b42b8c0ef5975369d052d57a28ea2
Kenneth Yan <kennyan11b@gmail.com>
2023-11-09 14:53:09 -0800

    Remove deleted files

--- MidtermProject-main/Alexandria/src/main/java/com/skilldistillery/alexandria/data/ClubDAO.java
+++
@@ -1,7 +0,0 @@
-package com.skilldistillery.alexandria.data;
-
-import com.skilldistillery.alexandria.entities.Club;
-
-public interface ClubDAO {
-	public Club addBookToClub(int clubId, int bookId);
-}
